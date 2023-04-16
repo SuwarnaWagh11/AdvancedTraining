599ec35c38a8eed76c0ffdc5075e2a17ac0a6f41
Suwarna_Wagh <2zo756s3g6av2j435b5zvaf26xp724eshz7svp5ae26xclxdcyva>
2023-04-16 12:11:39 +0200

    Added temporary files structure

---
+++ src/test/java/com/global/automation/apitest/Test1.java
@@ -0,0 +1,11 @@
+package com.global.automation.apitest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void demoTest(){
+        Assert.assertTrue(true);
+    }
+}
---
+++ src/test/java/com/global/automation/apitest/Test2.java
@@ -0,0 +1,11 @@
+package com.global.automation.apitest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test2 {
+    @Test
+    public void demoTest2(){
+        Assert.assertTrue(true);
+    }
+}
