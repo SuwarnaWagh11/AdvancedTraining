3ce4a7845092d4c4cc425b5af05e805d16981bba
Suwarna_Wagh <2zo756s3g6av2j435b5zvaf26xp724eshz7svp5ae26xclxdcyva>
2023-07-03 23:04:43 +0200

    Lastest comment

--- AutomationFramework/src/test/java/alltests/uitest/coreTest/DashboardPageTest.java
+++ AutomationFramework/src/test/java/alltests/uitest/coreTest/DashboardPageTest.java
@@ -9,28 +9,28 @@ import org.testng.annotations.Test;
 
 public class DashboardPageTest extends BaseClass {
 
-    private static final Logger LOGGER = LogManager.getLogger(DashboardPageTest.class);
+    //private static final Logger LOGGER = LogManager.getLogger(DashboardPageTest.class);
 
     @Test
     public void totalWidgetPresent(){
         dashboard.clickOnDemoDashboard();
         demoDashboard.getTotalWidgetPresetOnDemoDashboard();
         Assert.assertEquals(demoDashboard.getTotalWidgetPresetOnDemoDashboard(), 12);
-        BaseClass.logger1.info("Test for verifying total Widget Present on the dashboard");
+        //BaseClass.logger1.info("Test for verifying total Widget Present on the dashboard");
     }
     @DataProvider(name = "test-data")
     public Object[][] dataProvFunc(){
         return new Object[][]{
                 {"LAUNCH STATISTICS AREA"},{"LAUNCH STATISTICS BAR"},{"INVESTIGATED PERCENTAGE OF LAUNCHES"},
-                {"TEST CASES GROWTH TREND CHART"},{"OVERALL STATISTICS PANEL"},{"LAUNCHES DURATION CHART"},
-                {"OVERALL STATISTICS DONUT"},{"FAILED CASES TREND CHART"},{"LAUNCH TABLE"},
-                {"MOST FAILED TEST CASES"},{"PASSING RATE SUMMARY"},{"FLAKY TEST CASES"}
+                {"TEST CASES GROWTH TREND CHART"},{"OVERALL STATISTICS PANEL"}
         };
     }
-
+/*,{"LAUNCHES DURATION CHART"},
+    {"OVERALL STATISTICS DONUT"},{"FAILED CASES TREND CHART"},{"LAUNCH TABLE"},
+    {"MOST FAILED TEST CASES"},{"PASSING RATE SUMMARY"},{"FLAKY TEST CASES"}*/
     @Test(dataProvider ="test-data")
     public void searchDashboardNameDP(String keyWord){
-        BaseClass.logger1.info("Test for verifying the '"+keyWord+"' Of Widget Preset On Demo Dashboard.");
+        //BaseClass.logger1.info("Test for verifying the '"+keyWord+"' Of Widget Preset On Demo Dashboard.");
         dashboard.clickOnDemoDashboard();
         boolean isPresent = demoDashboard.verifyNamesOfWidgetPresetOnDemoDashboard(keyWord);
         Assert.assertTrue(isPresent);
